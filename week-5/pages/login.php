55206a5240b98b7f9fc18844b4c1274257cf1b65
Pat <142281446+xelfus23@users.noreply.github.com>
2023-12-15 22:51:57 +0800

    Update login.php

--- week-5/pages/login.php
+++ week-5/pages/login.php
@@ -1,5 +1,5 @@
 <?php  
-    include "../php/database/database.php"; 
+    include "../php/database/database.php"; //include this because we need to start session and we need the variables too
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,18 +11,23 @@
     <link rel="stylesheet" href="../css/gen.css">
 </head>
 <body>
+    
     <div class="login">
+        <!--Form starts -->
         <form method="post">
             <div class="back">
                 <a href="index.php">Back</a>
             </div>
             <div class="user-input">
+                <!--Username-->
                 <label for="name">Username</label>
                 <input type="text" name="username" id="name" autocomplete="off">
+                <!--Password-->
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password">
             </div>
             <div class="err">
+                <!--Sessions this will show when we submit-->
                 <?php 
                     if(isset($_SESSION['invalid'])){
                         echo $_SESSION['invalid'];
@@ -39,35 +44,45 @@
                 ?>
             </div>
             <div class="reg">
+                        <!--Register button-->
                 <p>Don't have an account yet? <a style="opacity: 1; text-decoration:underline;" href="register.php"> Register now!</a></p>
             </div>
             <div class="btn">
+                <!--Submit Button-->
                 <input class="btn-1" type="submit" name="login" value="Login">
             </div>
         </form>
+        <!-- Form ends -->
     </div>
 </body>
 </html>
-
+<!--Start PHP-->
 <?php 
-
+//if request post
     if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+        //store data from form to variable
         $username = $_POST['username'];
         $password = $_POST['password'];
 
         try{
+            //connect database
             $conn = new PDO("mysql:host=$db_host;dbname=$db_name",$db_username, $db_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+            //sql query
             $checkusername = $conn->prepare("SELECT * FROM users WHERE username = :username");
+            //bind parameters
             $checkusername->bindParam(':username', $username);
+            //execute
             $checkusername->execute();
+
+            //check if we can find the username in our database
             if($checkusername->rowCount() > 0){
                 $user = $checkusername->fetch(PDO::FETCH_ASSOC);
-
+                //if we found the username from our databse now we need to verify if the password match with the username we submit
                 if(password_verify($password, $user['password'])){
-                    $_SESSION['username'] = $username;
-                    header("location: ../pages/menu.php");
+                    $_SESSION['username'] = $username; //add session to use this to check if we logged in or not
+                    header("location: ../pages/menu.php"); //redirect to menu
                 }
                 else{
                     $_SESSION['invalid'] = "<div class = 'error'>Invalid username or password.</div>";
