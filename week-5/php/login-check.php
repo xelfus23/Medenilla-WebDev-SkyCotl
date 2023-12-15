5195f184b7d32554276b23180cce0776f8ae5083
Pat <142281446+xelfus23@users.noreply.github.com>
2023-12-15 22:42:20 +0800

    Update login-check.php

--- week-5/php/login-check.php
+++ week-5/php/login-check.php
@@ -1,4 +1,5 @@
 <?php 
+//check if the user is logged in if not they will redirect to login.php
     if(!isset($_SESSION['username'])){
         $_SESSION['log-check'] = "<div class = 'error'> Please log in.</div>";
         header('Location: ../pages/login.php');
